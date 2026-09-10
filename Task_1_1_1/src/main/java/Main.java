dcba60c12d020573caa6e048e49592f6c34d0fb0
nikitapodkolzin3745 <nikitapodkolzin3745@gmail.com>
2026-09-10 18:40:56 +0700

    try to pass the checkstyle2

--- Task_1_1_1/src/main/java/Main.java
+++ Task_1_1_1/src/main/java/Main.java
@@ -1,58 +1,58 @@
 import java.util.Arrays;
 
 public class Main {
-  /**
-   * Just main function...
-   * 
-   * @param args command line arguments
-   */
-  public static void main(String[] args) {
-  }
-
-  /**
-   * Sorts an array using heapsort
-   * 
-   * @param arr array to sort. mutable
-   */
-  static void sort(int[] arr) {
-    for (int i = arr.length / 2 - 1; i >= 0; i--) {
-      heapify(arr, arr.length, i);
+    /**
+    * Just main function...
+    * 
+    * @param args command line arguments
+    */
+    public static void main(String[] args) {
     }
 
-    for (int i = arr.length - 1 ; i > 0 ; i--) {
-      int tmp = arr[0];
-      arr[0] = arr[i];
-      arr[i] = tmp;
+    /**
+    * Sorts an array using heapsort
+    * 
+    * @param arr array to sort. mutable
+    */
+    static void sort(int[] arr) {
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            heapify(arr, arr.length, i);
+        }
 
-      heapify(arr, i, 0);
-    }
-  }
+        for (int i = arr.length - 1 ; i > 0 ; i--) {
+            int tmp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = tmp;
 
-  /**
-   * Restores the heap property for the subtree rooted at i.
-   * 
-   * @param arr array to heapify
-   * @param n size of the heap
-   * @param i root index
-   */
-  static void heapify(int[] arr, int n, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if (left < n && arr[largest] < arr[left]) {
-      largest = left;
-    }
-    if (right < n && arr[largest] < arr[right]) {
-      largest = right;
+            heapify(arr, i, 0);
+        }
     }
 
-    if (largest != i) {
-      int tmp = arr[largest];
-      arr[largest] = arr[i];
-      arr[i] = tmp;
+    /**
+    * Restores the heap property for the subtree rooted at i.
+    * 
+    * @param arr array to heapify
+    * @param n size of the heap
+    * @param i root index
+    */
+    static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
-      heapify(arr, n, largest);
+        if (left < n && arr[largest] < arr[left]) {
+            largest = left;
+        }
+        if (right < n && arr[largest] < arr[right]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int tmp = arr[largest];
+            arr[largest] = arr[i];
+            arr[i] = tmp;
+
+            heapify(arr, n, largest);
+        }
     }
-  }
 }
