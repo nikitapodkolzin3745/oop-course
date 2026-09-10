211771ef34a80908a5771295f0806e4a82bed895
nikitapodkolzin3745 <nikitapodkolzin3745@gmail.com>
2026-09-10 11:13:08 +0700

    1

--- Task_1_1_1/build.gradle.kts
+++ Task_1_1_1/build.gradle.kts
@@ -16,3 +16,16 @@ dependencies {
 repositories {
     mavenCentral()
 }
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
